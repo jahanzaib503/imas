3ab9b2190fdd3a1bf1e97d97a7686132de4e7e82
Ubaid37official <s.u.shah68@gmail.com>
2024-07-30 23:53:34 +0500

    made changes web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MedicalController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,77 +14,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-})->name('index');
-
+Route::get('/', function () { return view('index'); })->name('index');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Auth::routes(['register' => false]);
+Route::post('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
 
 // Route::get('/signup', function () {
 //     return view('signup');
 // })->name('signup');
-
-Route::get('/seafarers-medical', function () {
-    return view('seafarers-medical');
-})->name('seafarers-medical');
-
-Route::get('/ambulance-driver-medical', function () {
-    return view('ambulance-driver-medical');
-})->name('ambulance-driver-medical');
-
-Route::get('/taxi-driver-medical', function () {
-    return view('taxi-driver-medical');
-})->name('taxi-driver-medical');
-
-Auth::routes(['register' => false]);
-
-Route::post('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
+Route::get('/seafarers-medical', [MedicalController::class, 'seafarersMedical'])->name('seafarers-medical');
+Route::get('/ambulance-driver-medical', [MedicalController::class, 'ambulanceDriverMedical'])->name('ambulance-driver-medical');
+Route::get('/taxi-driver-medical', [MedicalController::class, 'taxiDriverMedical'])->name('taxi-driver-medical');
+Route::get('/hgv-driver-medical', [MedicalController::class, 'hgvDriverMedical'])->name('hgv-driver-medical');
+Route::get('/crane-operators-medical', [MedicalController::class, 'craneOperatorsMedical'])->name('crane-operators-medical');
+Route::get('/forklift-operator-medical', [MedicalController::class, 'forkliftOperatorMedical'])->name('forklift-operator-medical');
+Route::get('/bus-driver-medical', [MedicalController::class, 'busDriverMedical'])->name('bus-driver-medical');
+Route::get('/train-driver-medical', [MedicalController::class, 'trainDriverMedical'])->name('train-driver-medical');
+Route::get('/motorhome-drivers-medical', [MedicalController::class, 'motorhomeDriversMedical'])->name('motorhome-drivers-medical');
+Route::get('/sports-medicals', [MedicalController::class, 'sportsMedicals'])->name('sports-medicals');
+Route::get('/sickness-referrals', [MedicalController::class, 'sicknessReferrals'])->name('sickness-referrals');
+Route::get('/health-surveillance', [MedicalController::class, 'healthSurveillance'])->name('health-surveillance');
+Route::get('/pre-employment-medicals', [MedicalController::class, 'preEmploymentMedicals'])->name('pre-employment-medicals');
+Route::get('/safety-critical-medicals', [MedicalController::class, 'safetyCriticalMedicals'])->name('safety-critical-medicals');
 
 
-Route::get('/hgv-driver-medical', function () {
-    return view('hgv-driver-medical');
-})->name('hgv-driver-medical');
 
 
-Route::get('/crane-operators-medical', function () {
-    return view('crane-operators-medical');
-})->name('crane-operators-medical');
 
-Route::get('/forklift-operator-medical', function () {
-    return view('forklift-operator-medical');
-})->name('forklift-operator-medical');
-
-Route::get('/bus-driver-medical', function () {
-    return view('bus-driver-medical');
-})->name('bus-driver-medical');
-
-Route::get('/train-driver-medical', function () {
-    return view('train-driver-medical');
-})->name('train-driver-medical');
-
-Route::get('/motorhome-drivers-medical', function () {
-    return view('motorhome-drivers-medical');
-})->name('motorhome-drivers-medical');
-
-Route::get('/sports-medicals', function () {
-    return view('sports-medicals');
-})->name('sports-medicals');
-
-Route::get('/sickness-referrals', function () {
-    return view('sickness-referrals');
-})->name('sickness-referrals');
-
-Route::get('/health-surveillance', function () {
-    return view('health-surveillance');
-})->name('health-surveillance');
-
-Route::get('/pre-employment-medicals', function () {
-    return view('pre-employment-medicals');
-})->name('pre-employment-medicals');
-
-Route::get('/safety-critical-medicals', function () {
-    return view('safety-critical-medicals');
-})->name('safety-critical-medicals');
 
 Route::get('/book-medical', function () {
     return view('book-medical');
